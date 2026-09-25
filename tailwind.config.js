94fd1aaf0c7aecb7273cdf2bdd3f36ce9f7a990d
Mayank Pandey <pixelcraftanimations01@gmail.com>
2026-09-25 20:37:55 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,4 +0,0 @@
-tailwind.config = { darkMode:'class', theme:{ extend:{
-  fontFamily:{ display:['Space Grotesk','sans-serif'], body:['Inter','sans-serif'] },
-  colors:{ ink:'#0E1520', paper:'#FFFFFF', amber:'#E8A735', slate2:'#8EAAAD', slate3:'#5B6F72' }
-}}}
